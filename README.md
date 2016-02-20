8db4e3d4be3ebc368abab65ee10b6682c670f1bf
Shane Devane <shanedevane@gmail.com>
2016-02-20 10:23:37 +0000

    skeleton files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # trusted_email_validator
 Validates an email address, regex, smtp and assigns a level of 'trust'
+
---
+++ trusted_email_validator/trusted_email_validator.py
@@ -0,0 +1,12 @@
+__author__ = 'Shane Devane'
+
+
+class TrustedEmailValidator():
+
+    @staticmethod
+    def is_email_trusted():
+        pass
+
+    @staticmethod
+    def is_email_free():
+        pass
